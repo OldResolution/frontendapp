9923ab691cd7eac5aa3ac00ee31fd18ccfec14e3
OldResolution <140333635+OldResolution@users.noreply.github.com>
2024-12-10 05:18:53 +0530

    New Branch to resolve merge issues and working complete

--- Linguistic/src/module-info.java
+++ Linguistic/src/module-info.java
@@ -1,2 +1,10 @@
-module $MODULE_NAME$ {
+module frontendapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires java.sql;
+
+    exports Language to javafx.graphics;
+    opens Language to javafx.fxml;
 }
